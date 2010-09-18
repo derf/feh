4bd3b9ac28d66cd2d4efe1c86215f7ff5a1648f6
Daniel Friesel <derf@finalrewind.org>
2010-09-18 22:43:32 +0200

    support.c: Add missing break statement. Duh.

--- src/support.c
+++ src/support.c
@@ -59,6 +59,7 @@ void feh_wm_set_bg_file(char *file, unsigned char bgmode)
 			break;
 		case BG_MODE_FILL:
 			feh_wm_set_bg(file, im, 0, 0, 1, 0, 1);
+			break;
 		case BG_MODE_MAX:
 			feh_wm_set_bg(file, im, 0, 0, 2, 0, 1);
 			break;
