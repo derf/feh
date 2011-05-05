3cdc72ad068b9e63295e3d2f941da403690ad966
Daniel Friesel <derf@finalrewind.org>
2011-05-05 15:14:54 +0200

    scripts/update-todo: Pass file directly to git commit (thx mxey)

--- scripts/update-todo.sh
+++ scripts/update-todo.sh
@@ -1,7 +1,4 @@
 #!/bin/sh -e
 
-test -z "$(git ls-files --modified --deleted --others --exclude-standard)"
-
 ghi --repo=derf/feh --verbose list > TODO
-git add TODO
-git commit -m 'Update TODO (via github)'
+git commit -m 'Update TODO (via github)' TODO
