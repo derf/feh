f74385dec092224d046dddc02b731ee30405710f
Daniel Friesel <derf@finalrewind.org>
2013-09-26 18:36:57 +0200

    add "--info ;cmd" to disable info display on startup (closes #142)

--- src/options.c
+++ src/options.c
@@ -717,7 +717,10 @@ static void feh_parse_option_array(int argc, char **argv, int finalrun)
 			break;
 		case 234:
 			opt.info_cmd = estrdup(optarg);
-			opt.draw_info = 1;
+			if (opt.info_cmd[0] == ';')
+				opt.info_cmd++;
+			else
+				opt.draw_info = 1;
 			break;
 		case 235:
 			opt.force_aliasing = 1;
