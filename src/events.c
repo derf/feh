c0649c4b2a7c824abc743fa59457221e2bc3ab21
Daniel Friesel <derf@finalrewind.org>
2013-11-24 23:29:15 +0100

    thumbnail mode: if --action is set, run it instead of opening images

--- src/events.c
+++ src/events.c
@@ -378,8 +378,12 @@ static void feh_event_handle_ButtonRelease(XEvent * ev)
 				x /= winwid->zoom;
 				y /= winwid->zoom;
 				thumbfile = feh_thumbnail_get_file_from_coords(x, y);
-				if (thumbfile)
-					feh_thumbnail_show_fullsize(thumbfile);
+				if (thumbfile) {
+					if (opt.actions[0])
+						feh_action_run(thumbfile, opt.actions[0]);
+					else
+						feh_thumbnail_show_fullsize(thumbfile);
+				}
 			}
 		} else {
 			opt.mode = MODE_NORMAL;
