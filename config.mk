d89599dae94f47170548be5e25c903f6ab2c9a96
Ryan Schmidt <git@ryandesign.com>
2022-01-08 16:06:51 -0600

    Define _DARWIN_C_SOURCE for mkdtemp and mkstemps
    When _POSIX_C_SOURCE is defined, macOS hides the definitions of mkdtemp
    and mkstemps, among others. Define _DARWIN_C_SOURCE to counteract that.

--- config.mk
+++ config.mk
@@ -39,7 +39,7 @@ CFLAGS ?= -g -O2
 CFLAGS += -Wall -Wextra -pedantic
 
 # Settings for glibc >= 2.19 - may need to be adjusted for other systems
-CFLAGS += -std=c11 -D_POSIX_C_SOURCE=200809L -D_XOPEN_SOURCE=700
+CFLAGS += -std=c11 -D_POSIX_C_SOURCE=200809L -D_XOPEN_SOURCE=700 -D_DARWIN_C_SOURCE
 
 ifeq (${curl},1)
 	CFLAGS += -DHAVE_LIBCURL
