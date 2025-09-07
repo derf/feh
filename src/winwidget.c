derf/feh

commit 2a90af6d3709dd01c447f18db5109cab86eb4735

Fix offset calculation when image is larger than screen

src/winwidget.c
@@ -549,10 +549,14 @@ void winwidget_render_image(winwidget winwid, int resize, int force_alias)
 	calc_h = lround(winwid->im_h * winwid->zoom);
 	dw = (winwid->w - winwid->im_x);
 	dh = (winwid->h - winwid->im_y);
-	if (calc_w < dw)
+	if (calc_w < dw) {
 		dw = calc_w;
-	if (calc_h < dh)
+		dx = 0;
+	}
+	if (calc_h < dh) {
 		dh = calc_h;
+		dy = 0;
+	}
 	if (dw > winwid->w)
 		dw = winwid->w;
 	if (dh > winwid->h)

commit 4f3dbe00fd45ac09dc48987023a7cf89b70a19b7

Fix images not being centered when fully shown
This broke with 2a90af6d3709dd01c447f18db5109cab86eb4735.

src/winwidget.c
@@ -551,13 +551,13 @@ void winwidget_render_image(winwidget winwid, int resize, int force_alias)
 	dh = (winwid->h - winwid->im_y);
 	if (calc_w < dw) {
 		dw = calc_w;
-		if (!winwid->full_screen) {
+		if (dw >= winwid->w) {
 			dx = 0;
 		}
 	}
 	if (calc_h < dh) {
 		dh = calc_h;
-		if (!winwid->full_screen) {
+		if (dh >= winwid->h) {
 			dy = 0;
 		}
 	}